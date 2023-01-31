12cfdaf13d6b559eb63dac073e31738ce5a4f53b
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-01-31 21:07:42 +0000

    Add tasks

---
+++ Data types and variables Exercises/04. Sum of Chars/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _04._Sum_of_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfLines = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 0; i < numOfLines; i++)
+            {
+                // string --> char = Parse
+                char currentChar = char.Parse(Console.ReadLine());
+                sum += (int) currentChar;
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
