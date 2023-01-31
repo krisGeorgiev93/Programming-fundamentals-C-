61f5038390119f2155b0751823858f1b40751b21
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-01-31 21:06:23 +0000

    Add tasks

---
+++ Data types and variables Exercises/01. Integer Operations/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01._Integer_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+            int number3 = int.Parse(Console.ReadLine());
+            int number4 = int.Parse(Console.ReadLine());
+
+            int addOperation = number1 + number2;
+            int divideOperation = addOperation / number3;
+            int multiplyOperation = divideOperation * number4;
+            Console.WriteLine(multiplyOperation);
+
+        }
+    }
+}
