7809811dec2c3583a4676330c126c222104a55f9
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-01-31 21:06:50 +0000

    Add tasks

---
+++ Data types and variables Exercises/2. Sum Digits/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _2._Sum_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            do
+            {
+                sum += (number % 10); // returns the last digit of the "number"
+                number = number / 10; // deletes the last digit of "number"
+
+            }
+            while (number > 0);
+
+            Console.WriteLine(sum);
+        }
+    }
+}
