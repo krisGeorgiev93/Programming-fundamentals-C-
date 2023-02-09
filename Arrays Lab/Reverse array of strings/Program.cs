ea060645c714a165801e318f80e6119dce656cf6
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-02-09 21:53:54 +0000

    Add tasks

---
+++ Arrays Lab/Reverse array of strings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Reverse_Array_of_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] letters = Console.ReadLine().Split(" ").ToArray();
+
+            for (int i = letters.Length - 1; i >= 0; i--)
+            {
+                string currLetter = letters[i];
+                Console.Write(letters[i] + " ");
+            }
+        }
+    }
+}
+
