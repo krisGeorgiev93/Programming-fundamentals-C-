abf3461be3e082681bc05649d46ad04819af7501
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-02-10 15:29:48 +0000

    Add tasks

---
+++ Arrays Lab/Equal Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace Equal_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array1 = Console.ReadLine().Split().ToArray();
+            string[] array2 = Console.ReadLine().Split().ToArray();
+
+            int sum = 0;           
+
+            for (int i = 0; i < array2.Length; i++)
+            {                
+                    if (array1[i] == array2[i])
+                    {
+                    sum += int.Parse(array1[i]);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                        return;
+                    }                
+            }
+            Console.WriteLine($"Arrays are identical. Sum: {sum}");
+        }
+    }
+}
