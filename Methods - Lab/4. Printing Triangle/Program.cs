db1e521f2c8649cf9c5d3c3486ae03297f3d279e
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-02-11 21:25:38 +0000

    Add tasks

---
+++ Methods - Lab/4. Printing Triangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _4._Printing_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= input; i++) // tozi for otgovarq za redovete 
+            {
+                PrintColumns(i);
+            }
+
+            for (int i = input - 1; i >= 1; i--) // vtorite redove ( sled polovinata)
+            {
+                PrintColumns(i);
+
+                //1
+                //1 2
+                //1 2 3
+                //1 2
+                //1
+            }
+        }
+
+        private static void PrintColumns(int i)
+        {
+            for (int j = 1; j <= i; j++)
+            {
+                Console.Write(j + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
