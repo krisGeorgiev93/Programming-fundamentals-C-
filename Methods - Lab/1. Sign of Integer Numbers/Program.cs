d49a319cc460f27aa0b0e0a1a6e711ffb56855ca
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-02-11 21:23:43 +0000

    Add tasks

---
+++ Methods - Lab/1. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1._Sign_of_Integer_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            theCheckedNumber(inputNumber);
+        }
+
+        private static void theCheckedNumber(int inputNumber)
+        {
+            if (inputNumber > 0)
+            {
+                Console.WriteLine($"The number {inputNumber} is positive. ");
+            }
+            else if (inputNumber < 0)
+            {
+                Console.WriteLine($"The number {inputNumber} is negative. ");
+            }
+            else
+            {
+                Console.WriteLine($"The number {inputNumber} is zero. ");
+
+            }
+        }
+    }
+}
