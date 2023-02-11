4d26e37e13936044842cffe81433b41f79b470fb
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-02-11 21:26:32 +0000

    Add tasks

---
+++ Methods - Lab/6. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _6._Calculate_Rectangle_Area
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+           
+            double area = GetRectangleArea(a, b);
+            Console.WriteLine(area);
+        }
+
+        static double GetRectangleArea(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
