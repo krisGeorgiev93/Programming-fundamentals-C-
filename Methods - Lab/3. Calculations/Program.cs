0f3f0f24ff0e4241f76389179d3017bd5391f904
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-02-11 21:25:13 +0000

    Add tasks

---
+++ Methods - Lab/3. Calculations/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _3._Calculations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+            int number1 = int.Parse(Console.ReadLine());
+            int number2 = int.Parse(Console.ReadLine());
+
+            if (command == "add")
+            {
+                Add(number1, number2);
+            }
+            else if (command == "multiply")
+            {
+                Multiply(number1, number2);
+            }
+            else if (command == "subtract")
+            {
+                Subtract(number1, number2);
+            }
+            else if (command == "divide")
+            {
+                Divide(number1, number2);
+            }
+        }
+
+        private static void Divide(int number1, int number2)
+        {
+            Console.WriteLine(number1 / number2);
+        }
+
+        private static void Subtract(int number1, int number2)
+        {
+            Console.WriteLine(number1 - number2);
+        }
+
+        private static void Multiply(int number1, int number2)
+        {
+            Console.WriteLine(number1 * number2);
+        }
+
+        private static void Add(int number1, int number2)
+        {
+            Console.WriteLine(number1 + number2);
+        }
+    }
+}
