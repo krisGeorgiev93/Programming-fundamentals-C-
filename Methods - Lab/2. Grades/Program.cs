b6e41ca679aa1d02c7b56bb0aeecc779373475d4
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-02-11 21:24:38 +0000

    Add tasks

---
+++ Methods - Lab/2. Grades/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _2._Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+
+            PrintInWords(grade);
+        }
+
+        private static void PrintInWords(double grade)
+        {
+            if (grade >= 2 && grade <= 2.99)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (grade > 2.99 && grade <= 3.49)
+            {
+                Console.WriteLine("Poor");
+            }
+            else if (grade > 3.49 && grade <= 4.49)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (grade > 4.49 && grade <= 5.49)
+            {
+                Console.WriteLine("Very good");
+            }
+            else if (grade > 5.49 && grade <= 6.00)
+            {
+                Console.WriteLine("Excellent");
+            }
+        }
+    }
+}
