516061157c6efbbfcbad759486a9cea22ade7c5a
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-02-12 21:45:07 +0000

    Add tasks

---
+++ Methods - Lab/7. Repeat String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _7._Repeat_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int totalRepeats = int.Parse(Console.ReadLine());
+
+            string result = RepeatString(input, totalRepeats);
+            Console.WriteLine(result);
+        }
+
+        private static string RepeatString(string input, int totalRepeats)
+        {
+            string result = "";
+            for (int i = 1; i <= totalRepeats; i++)
+            {
+                result += input;
+            }
+            return result.ToString();
+        }
+    }
+}
