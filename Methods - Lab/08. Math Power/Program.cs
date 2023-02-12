9255ca34af2387da588e2dc8e036f7086470129c
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-02-12 21:45:30 +0000

    Add tasks

---
+++ Methods - Lab/08. Math Power/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseNumber = double.Parse(Console.ReadLine());
+            int powerNumber = int.Parse(Console.ReadLine());
+
+            double result = RaiseToPower(baseNumber, powerNumber);
+            Console.WriteLine(result);
+            
+        }
+
+        private static double RaiseToPower(double baseNumber, double powerNumber)
+        {
+            double result = 0d;
+            result = Math.Pow(baseNumber, powerNumber);
+            return result;
+        }
+    }
+}
