a5d3bd5d83630bc24e205c336b63f2e57a3756d6
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-02-12 21:46:33 +0000

    Add tasks

---
+++ Methods - Lab/11. Math Operations/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace _11._Math_Operations
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            int number1 = int.Parse(Console.ReadLine());
+            string operatorr = Console.ReadLine();
+            int number2 = int.Parse(Console.ReadLine());
+
+            
+           string result = GetTheResult(number1, operatorr, number2);
+
+            Console.WriteLine(result);
+            
+        }
+
+       static string GetTheResult(int number1, string operatorr, int number2)
+        {
+
+            double result = 0;
+            if (operatorr == "/")
+            {
+                result = number1 / number2;              
+
+            }
+            else if (operatorr == "*")
+            {
+                result = number1 * number2;                
+
+            }
+            else if (operatorr == "+")
+            {
+                result = number1 + number2;               
+
+            }
+            else if (operatorr == "-")
+            {
+                result = number1 - number2;               
+
+            }
+            return result.ToString();
+            
+        }
+    }
+}
