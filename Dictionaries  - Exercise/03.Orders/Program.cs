9ff22fa0a9be0d6bd44e6e828cec45b5796b860b
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-02-15 21:00:50 +0000

    Add tasks

---
+++ Dictionaries  - Exercise/03.Orders/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, double[]> items = new Dictionary<string, double[]>();
+
+            string command;
+            while ((command = Console.ReadLine()) != "buy")
+            {
+                string[] commandArgs = command.Split();
+                string product = commandArgs[0];
+                double price = double.Parse(commandArgs[1]);
+                double quantity = double.Parse(commandArgs[2]);
+
+                if (!items.ContainsKey(product))
+                {
+                    items.Add(product, new double[2]);
+                }
+                items[product][0] = price;
+                items[product][1] += quantity;
+            }
+            foreach (var product in items)
+            {
+                double totalPrice = product.Value[0] * product.Value[1];
+                Console.WriteLine($"{product.Key} -> {totalPrice:f2}");
+            }
+
+
+        }
+    }
+}
---
+++ Dictionaries  - Exercise/practice/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string> >courses = new Dictionary<string, List<string>>();
+
+            string command;
+            while ((command = Console.ReadLine())!= "end")
+            {
+                string[] commandArgs = command.Split(" : ");
+                string courseName = commandArgs[0];
+                string studentName = commandArgs[1];
+
+                if (!courses.ContainsKey(courseName))
+                {
+                    courses.Add(courseName,new List<string>());
+                }
+                courses[courseName].Add(studentName);
+            }
+
+
+            foreach (var kvp in courses)
+            {
+                string courseName = kvp.Key;
+                List<string> students = kvp.Value;
+                Console.WriteLine($"{courseName}: {students.Count}");
+                foreach (var student in students)
+                {
+                    Console.WriteLine($"-- {student}");
+                }
+            }
+
+        }
+    }
+}
