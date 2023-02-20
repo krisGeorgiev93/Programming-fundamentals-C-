fe02ff434350da16e58f46eb253429cd3dfb2662
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-02-20 22:40:04 +0000

    Add tasks

---
+++ Dictionaries - Lab/example for Sorted Dictionaries/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+public class WordsCountingWithSOrtedDictionary
+{
+    private static readonly string Text =
+    "She uchish li she bachkash li? Be kvo she bachkash " +
+    "be? Tui vashto uchene li e? Ia po-hubavo opitai da " +
+    "BACHKASH da se uchish malko! Uchish ne uchish trqbva " +
+    "da bachkash!";
+    public static void Main()
+    {
+        var wordOccurrences = GetWordOccurrences(Text);
+        PrintWordOccurrenceCount(wordOccurrences);
+    }
+    static IDictionary<string, int> GetWordOccurrences(string text)
+    {
+        string[] tokens = text.Split(' ', '.', ',', '-', '?', '!');
+        var words = new SortedDictionary<string, int>();
+        foreach (string word in tokens)
+        {
+            if (!string.IsNullOrEmpty(word.Trim()))
+            {
+                int count = 0;
+                words.TryGetValue(word, out count);
+                words[word] = count + 1;
+            }
+        }
+        return words;
+    }
+    static void PrintWordOccurrenceCount(
+    IDictionary<string, int> wordOccurrence)
+    {
+        foreach (var wordEntry in wordOccurrence)
+        {
+            Console.WriteLine("Word '{0}' occurs {1} time(s) in the text", wordEntry.Key, wordEntry.Value);
+        }
+    }
+}
