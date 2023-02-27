b0f8cc9a5562e1560a470121c9742b55add8d55f
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-02-27 18:29:02 +0000

    Add tasks

---
+++ Dictionaries, Lambda and LINQ/Fix Emails/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Fix_Emails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dataBook = new Dictionary<string, string>();
+
+            string name = Console.ReadLine();
+            while (name != "stop")
+            {
+                string email = Console.ReadLine();
+                string[] emailSplitted = email.Split(".");
+                if (emailSplitted[1] != "us" && emailSplitted[1] != "uk")
+                {
+                    dataBook.Add(name, email);
+                }
+                name = Console.ReadLine();
+            }
+            foreach (var item in dataBook)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
