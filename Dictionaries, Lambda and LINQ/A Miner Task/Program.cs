2f31f874de91c047ca49b4db44b17a26874a6ab6
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-02-27 18:28:40 +0000

    Add tasks

---
+++ Dictionaries, Lambda and LINQ/A Miner Task/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace A_Miner_Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string resource = Console.ReadLine();
+
+            Dictionary<string, int> data = new Dictionary<string, int>();
+            while (resource != "stop")
+            {
+                if (!data.ContainsKey(resource))
+                {
+                    data[resource] = 0;
+                }
+                int quantity = int.Parse(Console.ReadLine());
+                data[resource] += quantity;
+
+                resource = Console.ReadLine();
+            }
+            foreach (var item in data)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
