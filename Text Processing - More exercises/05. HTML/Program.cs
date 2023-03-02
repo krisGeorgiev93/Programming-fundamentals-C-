954e33fdadff734c9ea40faa6debcf777c42ca7c
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-03-02 21:24:56 +0000

    Add tasks

---
+++ Text Processing - More exercises/05. HTML/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void Main()
+    {
+        string title = Console.ReadLine();
+        string content = Console.ReadLine();
+        string comment = Console.ReadLine();
+
+        Console.WriteLine("<h1>");
+        Console.WriteLine(title);
+        Console.WriteLine("</h1>");
+
+        Console.WriteLine("<article>");
+        Console.WriteLine(content);
+        Console.WriteLine("</article>");
+
+        while (comment != "end of comments")
+        {
+            Console.WriteLine($"<div>");
+            Console.WriteLine($"    {comment}");
+            Console.WriteLine($"</div>");
+            comment = Console.ReadLine();
+        }
+    }
+}
