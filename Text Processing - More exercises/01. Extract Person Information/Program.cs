d0276df318a31ec4a9c7ee85147e4b8d7d1663cb
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-03-02 21:23:55 +0000

    Add tasks

---
+++ Text Processing - More exercises/01. Extract Person Information/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            string input = Console.ReadLine();
+            int indexOfA = input.IndexOf('@');
+            int indexOfB = input.IndexOf('|');
+            int indexOfC = input.IndexOf('#');
+            int indexOfD = input.IndexOf('*');
+
+            string name = input.Substring(indexOfA + 1, indexOfB - indexOfA - 1);
+            int age = int.Parse(input.Substring(indexOfC + 1, indexOfD - indexOfC - 1));
+            Console.WriteLine($"{name} is {age} years old.");
+        }
+    }
+}
