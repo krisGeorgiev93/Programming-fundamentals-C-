20392de3467ed47fbe2431b21985d33644af72df
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-03-02 21:24:43 +0000

    Add tasks

---
+++ Text Processing - More exercises/04. Morse Code Translator/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split(' ').ToArray();
+
+        foreach (var word in input)
+        {
+
+            string letter = word.ToString();
+            if (letter == ".-")
+            {
+                Console.Write("A");
+            }
+            else if (letter == "-...")
+            {
+                Console.Write("B");
+            }
+            else if (letter == "-.-.")
+            {
+                Console.Write("C");
+            }
+            else if (letter == "-..")
+            {
+                Console.Write("D");
+            }
+            else if (letter == ".")
+            {
+                Console.Write("E");
+            }
+            else if (letter == "..-.")
+            {
+                Console.Write("F");
+            }
+            else if (letter == "--.")
+            {
+                Console.Write("G");
+            }
+            else if (letter == "....")
+            {
+                Console.Write("H");
+            }
+            else if (letter == "..")
+            {
+                Console.Write("I");
+            }
+            else if (letter == ".---")
+            {
+                Console.Write("J");
+            }
+            else if (letter == "-.-")
+            {
+                Console.Write("K");
+            }
+            else if (letter == ".-..")
+            {
+                Console.Write("L");
+            }
+            else if (letter == "--")
+            {
+                Console.Write("M");
+            }
+            else if (letter == "-.")
+            {
+                Console.Write("N");
+            }
+            else if (letter == "---")
+            {
+                Console.Write("O");
+            }
+            else if (letter == ".--.")
+            {
+                Console.Write("P");
+            }
+            else if (letter == "--.-")
+            {
+                Console.Write("Q");
+            }
+            else if (letter == ".-.")
+            {
+                Console.Write("R");
+            }
+            else if (letter == "...")
+            {
+                Console.Write("S");
+            }
+            else if (letter == "-")
+            {
+                Console.Write("T");
+            }
+            else if (letter == "..-")
+            {
+                Console.Write("U");
+            }
+            else if (letter == "...-")
+            {
+                Console.Write("V");
+            }
+            else if (letter == ".--")
+            {
+                Console.Write("W");
+            }
+            else if (letter == "-..-")
+            {
+                Console.Write("X");
+            }
+            else if (letter == "-.--")
+            {
+                Console.Write("Y");
+            }
+            else if (letter == "--..")
+            {
+                Console.Write("Z");
+            }
+            else if (letter == "|")
+            {
+                Console.Write(" ");
+            }
+        }
+    }
+}
