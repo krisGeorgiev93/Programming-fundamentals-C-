c708addb579c9974483cbce2de8e830c3ec5f83b
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-03-02 21:24:11 +0000

    Add tasks

---
+++ Text Processing - More exercises/02. Ascii Sumator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Program
+{
+    static void Main()
+    {
+        int first = char.Parse(Console.ReadLine());
+        int second = char.Parse(Console.ReadLine());
+        string text = Console.ReadLine();
+        int sum = 0;
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            if (text[i] > first && text[i] < second)
+            {
+                sum += text[i];
+            }
+        }
+        Console.WriteLine(sum);
+    }
+}
