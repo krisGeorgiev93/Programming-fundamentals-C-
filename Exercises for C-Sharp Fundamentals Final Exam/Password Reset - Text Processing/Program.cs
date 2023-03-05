3b23f62f03de4eba3bdf475d44b0f7d49c02af61
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-03-05 20:50:55 +0000

    Add tasks

---
+++ Exercises for C-Sharp Fundamentals Final Exam/Password Reset - Text Processing/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Password_Reset___Text_Processing
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            string command;
+            while ((command = Console.ReadLine()) != "Done")
+            {
+                string[] commandArgs = command.Split(" ");
+
+                string commandType = commandArgs[0];
+                if (commandType == "TakeOdd")
+                {
+                    string rawPass = string.Empty;
+
+                    for (int i = 0; i < password.Length; i++)
+                    {
+                        if (i % 2 != 0)
+                        {
+                            rawPass += password[i];
+                        }
+
+                    }
+                    Console.WriteLine(rawPass);
+                    password = rawPass;
+                }
+                if (commandType == "Cut")
+                {
+                    int index = int.Parse(commandArgs[1]);
+                    int length = int.Parse(commandArgs[2]);
+                    password = password.Remove(index, length);
+                    Console.WriteLine(password);
+                }
+                if (commandType == "Substitute")
+                {                 
+
+                    if (password.Contains(commandArgs[1]))
+                    {
+                        string substring = commandArgs[1];
+                        string substitute = commandArgs[2];
+                        password = password.Replace(substring, substitute);                       
+                        Console.WriteLine(password);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Nothing to replace!");
+                    }
+                }
+            }
+            Console.WriteLine($"Your password is: {password}");
+        }
+    }
+}
