c1bc8400dc92673546ce8b87ad9a4874988a3577
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-03-09 22:59:31 +0000

    Add tasks

---
+++ Exercises for C-Sharp Fundamentals Final Exam/Fancy_Barcodes - Regex/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02._Fancy_Barcodes
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string pattern = @"(@#+)(?<product>[A-Z][A-Za-z0-9]{4,}[A-Z])(@#+)";
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                if (Regex.IsMatch(input, pattern))
+                {
+                    Match match = Regex.Match(input, pattern);
+                    string product = match.Groups[3].Value;
+                    string productGroup = string.Empty;
+
+                    foreach (char chr in product)
+                    {
+                        if (char.IsDigit(chr))
+                        {
+                            productGroup += chr;
+                        }
+                    }
+                    if (productGroup == string.Empty)
+                    {
+                        productGroup = "00";
+                    }
+                    Console.WriteLine($"Product group: {productGroup}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid barcode");
+                }
+            }
+        }
+    }
+}
