a457aa7a21e2a22740c9085f5aa5964fdfb9595c
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-03-10 22:05:39 +0000

    Add tasks

---
+++ Exercises for C-Sharp Fundamentals Final Exam/Destination Mapper - Regex/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Destination_Mapper___Regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"(\=|\/)[A-Z][A-z]{2,}\1";
+            List<string> destinations = new List<string>();
+
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+            MatchCollection matchCollection = regex.Matches(input);
+            string word = string.Empty;
+            int totalSum = 0;
+
+            foreach (Match curDestination in matchCollection)
+            {
+                foreach (char ch in curDestination.ToString())
+                {
+                    if (char.IsLetter(ch))
+                    {
+                        word += ch;
+                    }
+                }
+
+                totalSum += word.Length;
+                destinations.Add(word);
+                word = string.Empty;
+            }
+
+            Console.Write("Destinations: ");
+            Console.Write(string.Join(", ",destinations));
+            Console.WriteLine();
+            Console.WriteLine($"Travel Points: {totalSum}");
+        }
+    }
+}
