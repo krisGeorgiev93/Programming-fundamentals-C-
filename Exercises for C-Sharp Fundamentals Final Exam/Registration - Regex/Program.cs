373a93121e70746a24fbd6f2f85ee6e049456367
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-03-13 22:15:24 +0000

    Add tasks

---
+++ Exercises for C-Sharp Fundamentals Final Exam/Registration - Regex/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Registration___Regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string pattern = @"U\$(?<userName>[A-Z][a-z]{2,})U\$P@\$(?<password>[A-z]{5,}[0-9]{1,})P@\$";
+            List<string> validRegistrations = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                Regex regex = new Regex(pattern);
+                Match match = regex.Match(input);
+                if (match.Success)
+                {                   
+                        string userName = match.Groups["userName"].Value;
+                        string password = match.Groups["password"].Value;
+
+                        validRegistrations.Add(userName);
+
+                        Console.WriteLine("Registration was successful");
+                        Console.WriteLine($"Username: {userName}, Password: {password}");                    
+                }
+                else
+                {
+                    Console.WriteLine("Invalid username or password");
+                }
+            }
+
+
+            Console.WriteLine($"Successful registrations: {validRegistrations.Count}");
+            
+        }
+    }
+}
