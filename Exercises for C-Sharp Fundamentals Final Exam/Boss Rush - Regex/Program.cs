ad866e038af643d07a2d0410cf9227893d47eee4
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-03-26 18:17:44 +0100

    Add tasks

---
+++ Exercises for C-Sharp Fundamentals Final Exam/Boss Rush - Regex/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Boss_Rush___Regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string pattern = @"\|(?<name>[A-Z]{4,})\|\:\#(?<title>[A-z]+\s[A-z]+)\#";
+            Regex regex = new Regex(pattern);
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                Match match = regex.Match(input);
+                if (match.Success)
+                {
+                    string name = match.Groups["name"].ToString();
+                    string title = match.Groups["title"].ToString();
+
+                    int strength = name.Length;
+                    int armour = title.Length;
+                    Console.WriteLine($"{name}, The {title}");
+                    Console.WriteLine($">> Strength: {strength}");
+                    Console.WriteLine($">> Armour: {armour}");
+                }
+                else
+                {
+                    Console.WriteLine("Access denied!");
+                }
+            }
+        }
+    }
+}
