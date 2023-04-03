c823b47496438cdd7a775dc8e87f252fe9b40f26
krisGeorgiev93 <119761546+krisGeorgiev93@users.noreply.github.com>
2023-04-03 21:55:46 +0100

    add task

---
+++ Exercises for C-Sharp Fundamentals Final Exam/Message Decrypter/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Message_Decrypter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string pattern = @"^(\$|\%)([A-Z][a-z]{2,})\1\:\s(\[(\d+)\])\|(\[(\d+)\])\|(\[(\d+)\])\|$";
+
+
+            for (int i = 0; i < n; i++)
+            {
+                string command = Console.ReadLine();
+                Regex regex = new Regex(pattern);
+                Match match = regex.Match(command);
+                if (match.Success)
+                {
+                    string tag = match.Groups[2].Value;
+                    char firstChar = (char)int.Parse(match.Groups[4].Value);
+                    char secondChar = (char)int.Parse(match.Groups[6].Value);
+                    char thirdChar = (char)int.Parse(match.Groups[8].Value);
+
+                    char word = firstChar += secondChar += thirdChar;
+                    Console.WriteLine($"{tag}: {word}");
+                }
+                else
+                {
+                    Console.WriteLine("Valid message not found!");
+                }
+            }
+        }
+    }
+}
